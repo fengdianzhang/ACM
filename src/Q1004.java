3fbfa1af6088c4d128ef2dc2994de471185837f0
kunkka <fengdianzhang@gmail.com>
2016-03-08 00:44:30 +0800

    update 1004

--- src/Q1004.java
+++ src/Q1004.java
@@ -4,10 +4,21 @@ public class Q1004 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int result = 1;
-        for (int i = 0; i < n; i++) {
-            result = result * (n % 10) % 10;
+        int l = n % 10;
+        int r = l;
+        int circle = 0;
+        for (int i = 0; true; i++) {
+            r = r * l % 10;
+            if (r == l) {
+                circle = i + 1;
+                break;
+            }
         }
-        System.out.println(result);
+        n = n % circle + circle;
+        r = 1;
+        for (int i = 0; i < n; i++) {
+            r = r * l % 10;
+        }
+        System.out.println(r);
     }
 }
