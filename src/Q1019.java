0bb749f72ce1853bef93bc1b92b9c55d8712d36c
kunkka <fengdianzhang@gmail.com>
2016-03-10 23:58:10 +0800

    update 1019

--- src/Q1019.java
+++ src/Q1019.java
@@ -1,21 +1,38 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q1019 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] array = new int[n];
+        int[] orginal = new int[n], sorted = new int[n];
         for (int i = 0; i < n; i++) {
-            array[i] = in.nextInt();
+            orginal[i] = in.nextInt();
         }
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (array[i] > array[j]){
-                    count++;
-                }
+        sorted[0] = orginal[0];
+        for (int i = 1; i < n; i++) {
+            System.out.println(Arrays.toString(sorted));
+            int index = search(orginal[i], sorted);
+            count += index;
+            for (int j = i; j > index; j++) {
+                sorted[j] = sorted[j - 1];
             }
+            sorted[index] = orginal[i];
         }
         System.out.println(count);
+    }
+
+    private static int search(int a, int[] array) {
+        int start = 0, end = array.length - 1, middle;
+        while (start < end-1) {
+            middle = (start + end) / 2;
+            if (array[middle] < a) {
+                end = middle;
+            } else {
+                start = middle;
+            }
+        }
+        return end;
     }
 }
